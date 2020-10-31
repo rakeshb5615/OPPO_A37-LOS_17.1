952a22bf8db593c4ab42132ed9756a0611a6be9a
Deepak chaurasia <48019687+AndroNoman@users.noreply.github.com>
2020-10-31 18:03:58 +0530

    Targeted_ims-daemon_outside_init

--- config.fs
+++ config.fs
@@ -9,3 +9,15 @@ mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
+
+[vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_RADIO
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
+[vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_RADIO
+group: AID_RADIO
+caps: NET_BIND_SERVICE
