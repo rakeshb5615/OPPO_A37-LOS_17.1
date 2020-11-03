4a0f220d2cc3409b0ceb184683c03b9e244035ee
Deepak chaurasia <48019687+AndroNoman@users.noreply.github.com>
2020-11-03 14:59:32 +0530

    kick_forlater

--- config.fs
+++ config.fs
@@ -10,14 +10,3 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
-[vendor/bin/imsdatadaemon]
-mode: 0755
-user: AID_RADIO
-group: AID_RADIO
-caps: NET_BIND_SERVICE
-
-[vendor/bin/ims_rtp_daemon]
-mode: 0755
-user: AID_RADIO
-group: AID_RADIO
-caps: NET_BIND_SERVICE
